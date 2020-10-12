fff8b8168eeb65fbe830895f383201ddf7de6e29
carpdm-dev <sinnisingla@outlook.com>
2020-10-12 12:47:38 +0530

    added hover effects and added resume and fixed responsive bugs  when shrinked to small size

--- script/script.js
+++ script/script.js
@@ -2,4 +2,21 @@ $(document).ready(function(){
     $(".nav-link").on('click', function(){
         $(".navbar-toggler-icon").click();
     });
+    $(window).scroll(function() {
+        var position = $(this).scrollTop();
+
+        $('.section').each(function() {
+            var target = $(this).offset().top;
+            var id = $(this).attr('id');
+
+            if (position >= target - 300) {
+                $('.navbar > ul > li > a').removeClass('active');
+                $('.navbar > ul > li > a[href=\\#' + id + ']').addClass('active');
+        }
+        });
+    });
+    $(".nav-link").click(function(){
+        $('.navbar > ul > li > a').removeClass('active');
+        $(this).addClass('active');
+    });
 });
